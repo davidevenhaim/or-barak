4b98c2a7e3b5d7574d4aefdfb952840007143df9
David <david13abe@gmail.com>
2025-11-24 16:10:42 +0200

    small deletion

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -6,4 +6,3 @@ export type { Partner } from "./partnership-section";
 export { QuickLinksSection } from "./quick-links-section";
 export type { QuickLink } from "./quick-links-section";
 export { QuickLinkCard } from "./quick-link-card";
-export { NewsletterSection } from "./newsletter-section";
