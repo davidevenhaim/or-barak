94c7b070940cb0ed0decedb738113e9143aa9269
David <david13abe@gmail.com>
2025-11-24 16:15:14 +0200

    more metadata fixes

--- src/app/(home)/contact/page.tsx
+++ src/app/(home)/contact/page.tsx
@@ -1,4 +1,14 @@
 import Contact from "@/components/contact";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata() {
+  const t = await getTranslations();
+
+  return {
+    title: `${t("contact_title")} | ${t("site_title")}`,
+    description: t("contact_description")
+  };
+}
 
 const ContactPage = () => {
   return <Contact />;
